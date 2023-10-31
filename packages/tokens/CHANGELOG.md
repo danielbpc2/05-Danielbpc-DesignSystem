7d6d87c9548b9f66f1372c571906a00686472f2a
Daniel Carvalho <danielbpc2@hotmail.com>
2023-10-31 16:46:24 -0300

    feat: add new color to tokens

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @danielbpc-ui/tokens
 
+## 2.0.0
+
+### Major Changes
+
+- Add new Level10 gray color
+
 ## 1.0.1
 
 ### Patch Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
   white: '#FFF',
   black: '#000',
 
+  grayLvl10: '#aeaeae',
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
   gray400: '#7C7C8A',
